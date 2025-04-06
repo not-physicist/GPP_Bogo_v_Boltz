3541e8b5f20316c1f6bbb625d6b5c965c5a8312f
Chenhuan Wang <chenhuan@fastmail.com>
2025-04-06 19:02:59 +0200

    initial cleaning

--- run_cluster.jl
+++
@@ -1,8 +0,0 @@
-using Pkg
-
-Pkg.activate("./")
-
-using GPP_SUGRA
-
-@time TModes.save_eom(3.6, 0.0035, "data/TMode-0.0035/")
-@time TModes.save_f(0.0035, "data/TMode-0.0035/", num_mᵪ=10, num_k=50)
