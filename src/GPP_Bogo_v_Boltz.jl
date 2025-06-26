5e15357a95bdd49286c64754ade97140f338f879
Chenhuan Wang <chenhuan@fastmail.com>
2025-06-26 17:14:55 +0200

    forgot this one

--- src/GPP_Bogo_v_Boltz.jl
+++ src/GPP_Bogo_v_Boltz.jl
@@ -7,17 +7,20 @@ module GPP_Bogo_v_Boltz
 # export TModes
 
 include("common.jl")
+include("boltz.jl")
 include("pp.jl")
 include("eom.jl")
-include("boltz.jl")
 
 include("TModel/TModel.jl")
 using .TModel
+export TModel
 
 include("Chaotic2.jl")
 using .Chaotic2
+export Chaotic2 
+
 include("Chaotic4.jl")
 using .Chaotic4
-
+export Chaotic4
 
 end
